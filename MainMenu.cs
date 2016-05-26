520dc6ffe18e2eed2577ee81cb824f00dcddf312
YunhwanNam <namsky1411@hanmail.net>
2016-05-26 16:10:53 +0900

    게임의 메인메뉴 c# 스크립트
    향후 추가되고 제거될 부분이 상당히 많아서 현재는 개략적인 c# 스크립트

---
+++ MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public GUISkin skin;
+
+	void OnGUI()
+    {
+        GUI.skin = skin;
+        GUI.Label(new Rect(10, 10, 400, 75), "고-오급 공피하기");
+
+        if( GUI.Button(new Rect(380, 160, 120, 45), "Play"))
+        {
+            SceneManager.LoadScene(1);
+        }
+        if( GUI.Button(new Rect(380, 215, 120, 45), "How to Play"))
+        {
+
+        }
+        if( GUI.Button(new Rect(380, 270, 120, 45), "Special thanks..."))
+        {
+
+        }
+        if( GUI.Button(new Rect(380, 325, 120, 45), "End"))
+        {
+            Application.Quit();
+        }
+    }
+}
