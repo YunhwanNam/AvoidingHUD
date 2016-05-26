bb6a29de8ca5e46e15c3e45d705cb0c9fc5b3665
YunhwanNam <namsky1411@hanmail.net>
2016-05-26 16:16:35 +0900

    게임 내 랜덤적인 요소를 설정하는 c# 스크립트
    총알이 나올 확률을 시간이 지남에 따라서 증가시키고, 조건을 만족시키면 총알을 생성하고, 미니맵에 총알의 위치를 표시하도록 설정한다.

---
+++ GameRandom.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameRandom : MonoBehaviour {
+    public Image pfEnemy;
+    public MiniMap map;
+    public Rigidbody pfBullet;
+    public Transform gunEnd;
+
+    private int randomNumber = 100;
+    private float startTime = 0f;
+    private int range;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        startTime += Time.deltaTime;
+        if (startTime / 4.0 == 0.0)
+            randomNumber--;
+        range = Random.Range(1, randomNumber);
+
+        if(range == 1)
+        {
+            Image enemyInstance;
+            enemyInstance = Instantiate(pfEnemy);
+            enemyInstance.transform.parent = map.transform;
+            Rigidbody bulletInstance;
+            bulletInstance = Instantiate(pfBullet, gunEnd.position, Quaternion.identity) as Rigidbody;
+            bulletInstance.AddForce(gunEnd.forward * 1000);
+
+            enemyInstance.GetComponent<Blip>().Target = bulletInstance.transform;
+        }
+	}
+}
