15f5adfa41c1a0840742cb30d321e3e5236990b2
YunhwanNam <namsky1411@hanmail.net>
2016-05-26 16:24:38 +0900

    미니맵에 표시되는 Target의 위치에 따른 Object의 위치를 설정해주는 함수.
    Blip과 Minimap 두 개의 c# 스크립트를 통해서 미니맵을 완성하고, 캐릭터가 회전하더라도, 미니맵 역시 회전하는 효과를 얻게 된다.

---
+++ MiniMap.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniMap : MonoBehaviour {
+
+    public Transform Target;
+    public float zoomLevel = 10f;
+    public bool LockRotation = false;
+
+    Vector2 XRotation = Vector2.right;
+    Vector2 YRotation = Vector2.up;
+
+    void LateUpdate()
+    {
+        XRotation = new Vector2(Target.right.x, -Target.right.z);
+        YRotation = new Vector2(-Target.forward.x, Target.forward.z);
+    }
+
+    public Vector2 TransformPosition(Vector3 position)
+    {
+        Vector3 offset = position - Target.position;
+        Vector2 newPosition = offset.x * XRotation;
+        newPosition += offset.z * YRotation;
+        newPosition *= zoomLevel;
+        return newPosition;
+    }
+
+    public Vector3 TransformRotation(Vector3 rotation)
+    {
+        if (LockRotation)
+            return new Vector3(0, 0, -rotation.y);
+        else
+            return new Vector3(0, 0, Target.eulerAngles.y - rotation.y);
+    }
+
+    public Vector2 MoveInside(Vector2 point)
+    {
+        Rect mapRect = GetComponent<RectTransform>().rect;
+        point = Vector2.Max(point, mapRect.min);
+        point = Vector2.Min(point, mapRect.max);
+        return point;
+    }
+}
