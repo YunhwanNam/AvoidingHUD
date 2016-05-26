ddb3c9e73a17bf0ffcf3fec178f4ed80031382e4
YunhwanNam <namsky1411@hanmail.net>
2016-05-26 16:09:19 +0900

    총알이 제거되는 시간에 대한 c# 스크립트
    초기값은 10초이고, 매 초당 0.1초씩 생존시간이 늘어남

---
+++ bulletDeath.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletDeath : MonoBehaviour {
+
+    private float lifeTime;
+    private float startTime = 0f;
+
+	// Update is called once per frame
+	void Update () {
+        startTime += Time.deltaTime;
+
+        lifeTime = 10f + (startTime / 10f);
+        if(startTime > lifeTime)
+        {
+            Destroy(gameObject, 0.1f);
+        }
+	}
+}
