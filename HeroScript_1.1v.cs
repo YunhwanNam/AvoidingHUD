28c545cc0dcd1a2a4086d83b8126b75140c4301f
YunhwanNam <namsky1411@hanmail.net>
2016-05-31 21:34:37 +0900

    Hero 스크립트 1.1v
    변경점 : Hero의 속력이 1차함수처럼 시간에 따라서 일정하게 증가하는 것이 아닌, 로그함수와 같이 증가하도록 바꾸었습니다.
    이를 통해서 속력이 주체할 수 없을정도로 빨라지는 것을 막아줍니다.

---
+++ HeroScript_1.1v.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroScript : MonoBehaviour {
+    public GameManager manager;
+    public GameObject deathParticles;
+    public GameObject Hero;
+
+    private float lifeTime = 0.1f;
+    private float speed = 4.0f;
+    private float zeroBase = 0.5f;
+    private float rotSpeed = 150f;
+    private float currentTime = 0.0f;
+
+    private Vector3 input;
+    private Vector3 spawn;
+    
+	// Use this for initialization
+	void Start () {
+        spawn = transform.position;
+        manager = manager.GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime += Time.deltaTime;
+
+        if(Mathf.Log(1.0f + currentTime, 2) > zeroBase)
+        {
+            speed += 4.0f;
+            zeroBase += 0.5f;
+            print(speed);
+        }
+
+        float amtToMove = speed * Time.deltaTime;
+        float amtToRot = rotSpeed * Time.deltaTime;
+        float ang = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * amtToMove);
+        transform.Rotate(Vector3.up * ang * amtToRot);
+
+        if(transform.position.y < -1)
+        {
+            Die();
+        }
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.transform.tag == "Enemy")
+        {
+            Die();
+        }
+
+        if(other.transform.tag == "Wall")
+        {
+            Hero.transform.forward = -Hero.transform.forward;
+        }
+    }
+    
+    void Die()
+    {
+        Instantiate(deathParticles, transform.position, Quaternion.identity);
+        transform.position = spawn;
+        Destroy(gameObject, lifeTime);
+        manager.completed = true;
+        manager.showWinScreen = true;
+    }
+}
