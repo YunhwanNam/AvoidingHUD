a21c4584ef2a0eddd6ff5a6f203421e371a9fc6d
YunhwanNam <namsky1411@hanmail.net>
2016-06-01 18:28:21 +0900

    총알이 발사되는 확률에 대한 스크립트
    변경점 : 총알이 나오는 확률이 변경되었습니다. 4초, 5초, 6초... 마다 총알이 나올 수 있는 범위가 1씩 증가합니다.(총 100까지)

---
+++ GameRandom_1.1v.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameRandom : MonoBehaviour {
+    public Image pfEnemy;
+    public MiniMap map;
+    public Rigidbody pfBullet;
+    public Transform gunEnd;
+    
+    private float startTime = 0f;
+    private int range;
+    private float last = 1f;
+    private float temp = 1f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        startTime += Time.deltaTime;
+        if (startTime >= 4f)
+        {
+            last += 1f;
+            startTime = 0.0f - temp;
+            temp += 1f;
+        }
+
+        range = Random.Range(1, 100);
+
+        if (range <= last)
+        {
+            Image enemyInstance;
+            enemyInstance = Instantiate(pfEnemy);
+            enemyInstance.transform.parent = map.transform;
+            Rigidbody bulletInstance;
+            bulletInstance = Instantiate(pfBullet, gunEnd.position, Quaternion.identity) as Rigidbody;
+            bulletInstance.AddForce(gunEnd.forward * 1000);
+
+            enemyInstance.GetComponent<Blip>().Target = bulletInstance.transform;
+        }
+	}
+}
