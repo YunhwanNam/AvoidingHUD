56e4144f23f58a2c0da36202abf2e7e44f2d7b8b
YunhwanNam <namsky1411@hanmail.net>
2016-06-01 18:17:48 +0900

    Hero의 방향을 바꾸는 스크립트
    Hero를 클릭할 시 17도씩 각도를 변화시켜준다.

---
+++ JoyStick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoyStick : MonoBehaviour {
+
+	public void OnMouseDown()
+    {
+        Debug.Log("Click");
+        transform.Rotate(new Vector3(0, 17, 0));
+    }
+}
