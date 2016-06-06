5524a147af99c5b8ff48aad80d6dd3feefa93d3a
YunhwanNam <namsky1411@hanmail.net>
2016-06-06 23:33:50 +0900

    How to Play에 대한 스크립트
    How to Play를 넘기기 위한 스크립트입니다.

---
+++ Demonstration1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Demonstration1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(Screen.width / 4 * 3, Screen.height / 6 * 5, Screen.width / 5, Screen.height / 10), "다음페이지로"))
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
+}
---
+++ Demonstration3.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Demonstration3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 4 * 3, Screen.height / 6 * 5, Screen.width / 5, Screen.height / 10), "초기 화면으로"))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
