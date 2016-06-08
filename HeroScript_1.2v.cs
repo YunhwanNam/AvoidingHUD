cf0c5828abbbedb730a6346fa846866d934e4b08
YunhwanNam <namsky1411@hanmail.net>
2016-06-08 20:56:34 +0900

    Hero 스크립트 1.2v
    수정점 : 컴퓨터 내에서 좌우 방향키를 누르면 회전하는 함수와 그에 관련된 모든 변수를 제거했습니다.
    
    벽과 충돌시 마치 반사되도록 하는 효과가 제대로 작동하도록 변경시켰습니다.

---
+++ HeroScript_1.2v.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroScript : MonoBehaviour {
+    public GameManager manager;
+    public GameObject deathParticles;
+    public GameObject Hero;
+
+    private float lifeTime = 0.1f;
+    private float speed = 4.0f;
+    private float zeroBase = 0.5f;
+    private float currentTime = 0.0f;
+
+    private Vector3 input;
+    private Vector3 spawn;
+
+    void Start()
+    {
+        spawn = transform.position;
+        manager = manager.GetComponent<GameManager>();
+    }
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if (Mathf.Log(1.0f + currentTime, 2) > zeroBase)
+        {
+            speed += 4.0f;
+            zeroBase += 0.5f;
+        }
+
+        float amtToMove = speed * Time.deltaTime;
+        transform.Translate(Vector3.forward * amtToMove);
+        if (transform.position.y < -1)
+        {
+            Die();
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.tag == "Enemy")
+        {
+            Die();
+        }
+
+        if (other.transform.tag == "Wall1")
+        {
+            Hero.transform.forward = new Vector3(Hero.transform.forward.x, Hero.transform.forward.y, -Hero.transform.forward.z);
+        }
+
+        if(other.transform.tag == "Wall2")
+        {
+            Hero.transform.forward = new Vector3(-Hero.transform.forward.x, Hero.transform.forward.y, Hero.transform.forward.z);
+        }
+    }
+
+    void Die()
+    {
+        Instantiate(deathParticles, transform.position, Quaternion.identity);
+        transform.position = spawn;
+        Destroy(gameObject, lifeTime);
+        manager.completed = true;
+        manager.showWinScreen = true;
+    }
+}
