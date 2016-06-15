dd533dd8b17c4c0b278872b7ff3e0b2e75f12472
YunhwanNam <namsky1411@hanmail.net>
2016-06-15 13:42:33 +0900

    Object를 드래그하면 좌우로 회전하는 script
    클릭 여부와 드래그 여부를 판별해서 y축 방향으로 드래그한 만큼 회전시키는 스크립트.

---
+++ Dragging.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dragging : MonoBehaviour
+{
+    private float _sensitivity;
+    private Vector3 _mouseReference;
+    private Vector3 _mouseOffset;
+    private Vector3 _rotation;
+    private bool _isRotating;
+
+    void Start()
+    {
+        _sensitivity = 0.3f;
+        _rotation = Vector3.zero;
+    }
+
+    void Update()
+    {
+        if (_isRotating)
+        {
+            _mouseOffset = (Input.mousePosition - _mouseReference);
+            _rotation.y = (_mouseOffset.x + _mouseOffset.y) * _sensitivity;
+            transform.Rotate(_rotation);
+            _mouseReference = Input.mousePosition;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        _isRotating = true;
+        _mouseReference = Input.mousePosition;
+    }
+
+    void OnMouseUp()
+    {
+        _isRotating = false;
+    }
+
+}
