f7752784016f77040912a71c3f91292134baf5e1
YunhwanNam <namsky1411@hanmail.net>
2016-06-15 14:19:24 +0900

    총알이 발사되는 것에 대한 스크립트
    변경점 : 이전에는 일정 확률로 총알을 발사시켰는데, 이번 스크립트부터는 몇초마다 총알을 발사하고, 시간이 지남에 따라서 총알이 발사되고 나서 다시 발사될때까지 걸리는 시간이 점점 줄어듭니다.

---
+++ GameRandom_1.2v.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameRandom : MonoBehaviour {
+    public Image pfEnemy;
+    public MiniMap map;
+    public Rigidbody pfBullet;
+    public Transform gunEnd;
+    
+    private float currentTime = 0f;
+    private float tempTime = 0f;
+    private float bulletDecrease = 0.1f;
+    private float bulletFire = 1f;
+    private float logNum = 1f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime += Time.deltaTime;
+        tempTime += Time.deltaTime;
+
+        if (Mathf.Log(currentTime, 2)> logNum)
+        {
+            bulletFire -= bulletDecrease;
+            logNum += 1.0f;
+            bulletDecrease *= 0.9f;
+        }
+
+        if(tempTime > bulletFire)
+        {
+            Image enemyInstance;
+            enemyInstance = Instantiate(pfEnemy);
+            enemyInstance.transform.parent = map.transform;
+            Rigidbody bulletInstance;
+            bulletInstance = Instantiate(pfBullet, gunEnd.position, Quaternion.identity) as Rigidbody;
+            bulletInstance.AddForce(gunEnd.forward * 1000);
+            enemyInstance.GetComponent<Blip>().Target = bulletInstance.transform;
+
+            tempTime = 0.0f;
+        }
+	}
+}
